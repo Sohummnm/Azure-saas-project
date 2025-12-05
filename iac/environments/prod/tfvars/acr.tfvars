1fa0f6679b0ad534a9f2a88aff61c47a801196ff
Soham <sohamkanoj55@gmail.com>
2025-12-05 19:27:05 +0530

    whole acr module, tfvars, backend with workflows

---
+++ iac/environments/prod/tfvars/acr.tfvars
@@ -0,0 +1,7 @@
+acr_name = "azure-saas-project-acr-prod"
+sku      = "Standard"
+
+tags = {
+  environment = "prod"
+  owner       = "platform-team"
+}
--- iac/modules/aks/outputs.tf
+++ iac/modules/aks/outputs.tf
@@ -14,3 +14,6 @@ output "aks_kube_config" {
 output "aks_user_node_pool_name" {
   value = azurerm_kubernetes_cluster_node_pool.user_node_pool.name
 }
+output "principal_id" {
+  value = azurerm_kubernetes_cluster.aks_cluster.principal_id
+}
